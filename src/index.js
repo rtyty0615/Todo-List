7bc83d20106a0fce1a1f15098b03c6b61bbe32cf
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-02-27 14:18:03 +0800

    Add icon svg files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { greeting } from "./project.js";
-import { checkAllIcon } from './icons.js';
+import * as Icons from './icons.js';
+
+const HeaderIcon = Icons.BigIcon;
 
 console.log(greeting);
